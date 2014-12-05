5aaa1973843022f3d3179d1e966315e9f991830b
David Janes <davidjanes@davidjanes.com>
2014-12-05 10:23:28 -0500

    IOTDB example

---
+++ samples/lights.js
@@ -0,0 +1,24 @@
+/*
+ *  Turn on all the lights in the basement 
+ *  10m before sunset and 
+ *  turn them off 10m after sunrise.
+ */
+
+var iotdb = require('iotdb')
+var timers = require('iotdb-timers');
+
+var lights = iotdb
+    .iot()
+    .connect()
+    .with_facet(":device.lighting")
+    .with_floor("Basement")
+    ;
+
+var do_lights = function() {
+    lights.set(":on", sunset.isAfter() || sunrise.isBefore());
+}
+
+var sunset = timers.sunset(-10 * 60, do_lights);
+var sunrise = timers.sunrise(10 * 60, do_lights);
+
+do_lights();
